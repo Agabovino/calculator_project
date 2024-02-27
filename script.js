f9bc008f5f312a1b7bc2e1a7ee958741cf444a6e
Agabovino <114683437+Agabovino@users.noreply.github.com>
2024-02-27 11:55:27 -0300

    add listerners to key btns

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let displayContent = document.querySelector(".inner-display-content");
+
+let powerBtn = document.querySelector(".power");
+let addBtn = document.querySelector(".add");
+
+let numberKeyArray = document.getElementsByClassName("numberKeys");
+numberKeyArray = Array.from(numberKeyArray);
+
+
+let oneBtn = document.querySelector(".one");
+console.log(oneBtn.textContent);
+
+let isOn = false;
+let memoryValue;
+
+
+
+/*----------FUNCTIONS----------*/
+
 function add(){
 
 }
@@ -7,9 +26,38 @@ function subtract(){
 }
 
 function multiply(){
-    
+
 }
 
 function divide(){
 
 }
+
+/*----------EVENT LISTENERS----------*/
+
+powerBtn.addEventListener("click", ()=>{
+
+    if(isOn == true){
+        displayContent.textContent = "";
+        isOn = false;
+    }else{
+        displayContent.textContent = "0";
+        isOn = true;
+    }
+    
+});
+
+addBtn.addEventListener("click", ()=>{
+
+});
+
+/*----------NUMBERS BTNS EVENT LISTENERS----------*/
+
+numberKeyArray.map((currentBtn)=>{
+   
+        currentBtn.addEventListener("click", ()=> {
+            if(isOn){
+                displayContent.textContent += currentBtn.textContent;
+            }
+        });
+});
